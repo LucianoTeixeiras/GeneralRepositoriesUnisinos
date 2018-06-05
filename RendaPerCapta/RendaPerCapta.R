b0b563c95874ddd8408677d305f87d28421c6239
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-06-05 00:01:34 -0300

    Commit por hoje

--- RendaPerCapta/RendaPerCapta.R
+++ RendaPerCapta/RendaPerCapta.R
@@ -1,5 +1,13 @@
 #Load Data Set
 
 library(readxl)
-atlas2013_dadosbrutos_pt <- read_excel("~/GitHub/PosUnisinosEstatisticaAplicada/RendaPerCapta/atlas2013_dadosbrutos_pt.xlsx")
-View(atlas2013_dadosbrutos_pt)
+dadosbrutos <- read_excel("~/GitHub/PosUnisinosEstatisticaAplicada/RendaPerCapta/atlas2013_dadosbrutos_pt.xlsx")
+View(dadosbrutos)
+
+dadosbrutos
+
+summary(dadosbrutos)
+
+head(dadosbrutos)
+
+#Markdown
