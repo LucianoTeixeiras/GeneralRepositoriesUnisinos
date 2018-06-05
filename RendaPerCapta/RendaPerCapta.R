7d12596c7915a1175726caa7f2ea9b1504789a22
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-06-04 22:45:26 -0300

    Commit do primeiro script

---
+++ RendaPerCapta/RendaPerCapta.R
@@ -0,0 +1,5 @@
+#Load Data Set
+
+library(readxl)
+atlas2013_dadosbrutos_pt <- read_excel("~/GitHub/PosUnisinosEstatisticaAplicada/RendaPerCapta/atlas2013_dadosbrutos_pt.xlsx")
+View(atlas2013_dadosbrutos_pt)
