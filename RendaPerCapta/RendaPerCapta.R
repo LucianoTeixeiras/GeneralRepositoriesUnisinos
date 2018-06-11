4b66a63d4ec136aecb306b1e19c18e49477d9282
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-06-10 22:36:48 -0300

    Commit inicial windows sem sucesso

--- RendaPerCapta/RendaPerCapta.R
+++ RendaPerCapta/RendaPerCapta.R
@@ -1,7 +1,7 @@
 #Load Data Set
 
 library(readxl)
-dadosbrutos <- read_excel("~/GitHub/PosUnisinosEstatisticaAplicada/RendaPerCapta/atlas2013_dadosbrutos_pt.xlsx")
+dadosbrutos <- read_excel("~/GitHub/PosUnisinosEstatisticaAplicada/RendaPerCapta/atlas2013_municipio.xlsx")
 View(dadosbrutos)
 
 dadosbrutos
@@ -11,3 +11,28 @@ summary(dadosbrutos)
 head(dadosbrutos)
 
 #Markdown
+
+# Variaveis
+Y = RDCP
+
+#leitura da base atlas completa para municípios
+library(readxl)
+RDPC_municipio <- read_excel("~/GitHub/PosUnisinosEstatisticaAplicada/RendaPerCapta/atlas2013_municipio.xlsx")
+#atlas_munic <- atlas2013_dadosbrutos_pt_somente_municipio
+View(RDPC_municipio)
+
+#Divisão da base do site por ano
+atlas_m1991<- subset.data.frame(atlas_munic, ANO==1991)
+atlas_m2000<- subset.data.frame(atlas_munic, ANO==2000)
+atlas_m2010<- subset.data.frame(atlas_munic, ANO==2010)
+
+#inclusão do idh2010 na base do ano 2000
+idhm_1991 <- c(atlas_m1991$IDHM)
+idhm_2000 <- c(atlas_m2000$IDHM)
+Y_idhm_2010 <- c(atlas_m2010$IDHM)
+atlas_m2000<- data.frame(atlas_m2000,Y_idhm_2010)
+
+#Histograma de algumas variáveis
+hist(atlas_m2000$ESPVIDA)
+hist(atlas_m2000$GINI)
+hist(atlas_m2000$Y_idhm_2010)
